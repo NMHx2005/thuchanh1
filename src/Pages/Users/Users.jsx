4b054407742e030d01431419452e44745c7f8036
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2025-08-27 19:43:59 +0700

    update create user

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,6 +1,7 @@
-import { Button, Drawer, Space, Table, Modal, Input } from 'antd';
+import { Button, Drawer, Space, Table, Modal, Input, message, Spin } from 'antd';
 import { useEffect, useState } from 'react';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 const Users = () => {
     const [result, setResult] = useState([]);
     const [open, setOpen] = useState(false);
@@ -10,8 +11,14 @@ const Users = () => {
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [messageApi, contextHolder] = message.useMessage();
 
 
+    // tự động reload lại table sau khi thêm mới
+    // THêm thông báo
+    // chưa xóa dữ liệu table sau khi tạo
+    // loading khi lấy dữ liệu
 
     const showModal = () => {
         setIsModalOpen(true);
@@ -33,8 +40,26 @@ const Users = () => {
             )
         })
         const result2 = await res.json();
-        console.log(result2);
+        messageApi.success("Thêm mới thành công");
+        setFullName("");
+        setEmail("");
+        setPhone("");
+        setPassword("");
+        await getData();
+
     };
+    function delay(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    const getData = async () => {
+        setIsLoading(true);
+        await delay(2000);
+        const res = await fetch('http://localhost:8080/api/v1/user');
+        const result2 = await res.json();
+        console.log(result2.data);
+        setResult(result2.data);
+        setIsLoading(false);
+    }
     const handleCancel = () => {
         setIsModalOpen(false);
     };
@@ -45,12 +70,6 @@ const Users = () => {
     // Deleted
 
     useEffect(() => {
-        const getData = async () => {
-            const res = await fetch('http://localhost:8080/api/v1/user');
-            const result2 = await res.json();
-            console.log(result2.data);
-            setResult(result2.data);
-        }
         getData();
     }, []);
 
@@ -91,50 +110,55 @@ const Users = () => {
     };
 
     return (
-        <div className="p-4">
-            <div className='flex justify-between items-center mb-4'>
-                <h1 className="text-2xl font-bold">Users Table</h1>
-                <Button type="primary" onClick={showModal}>+ Add User</Button>
+        <>
+            <div className="p-4">
+                <div className='flex justify-between items-center mb-4'>
+                    <h1 className="text-2xl font-bold">Users Table</h1>
+                    <Button type="primary" onClick={showModal}>+ Add User</Button>
+                </div>
+
+                {
+                    isLoading ? <Spin /> : <Table columns={columns} rowKey="_id" dataSource={result} />
+                }
+                <Drawer
+                    title="Thông tin người dùng"
+                    closable={{ 'aria-label': 'Close Button' }}
+                    onClose={onClose}
+                    open={open}
+                >
+                    {currentUser ?
+
+                        <div>
+                            <h1>Tên người dùng: {currentUser.fullName}</h1>
+                            <h1>Email: {currentUser.email}</h1>
+                            <h1>Role: {currentUser.role}</h1>
+                            <h1>Id: {currentUser._id}</h1>
+                        </div>
+                        :
+                        <div>Không có giá trị</div>
+                    }
+                </Drawer>
+
+
+                <Modal
+                    title="Thêm người dùng"
+                    closable={{ 'aria-label': 'Custom Close Button' }}
+                    open={isModalOpen}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                >
+                    <div>Tên người dùng</div>
+                    <Input placeholder="Tên người dùng" name='fullName' value={fullName} onChange={(e) => setFullName(e.target.value)} />
+                    <div>Email</div>
+                    <Input placeholder="Email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <div>password</div>
+                    <Input type='password' placeholder="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <div>phone</div>
+                    <Input placeholder="phone" name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} />
+                </Modal>
             </div>
-
-            <Table columns={columns} rowKey="_id"  dataSource={result} />
-            <Drawer
-                title="Thông tin người dùng"
-                closable={{ 'aria-label': 'Close Button' }}
-                onClose={onClose}
-                open={open}
-            >
-                { currentUser ? 
-               
-                <div>
-                        <h1>Tên người dùng: {currentUser.fullName}</h1>
-                        <h1>Email: {currentUser.email}</h1>
-                        <h1>Role: {currentUser.role}</h1>
-                        <h1>Id: {currentUser._id}</h1>
-                </div>    
-                :
-                <div>Không có giá trị</div>
-            }
-            </Drawer>
-
-
-            <Modal
-                title="Thêm người dùng"
-                closable={{ 'aria-label': 'Custom Close Button' }}
-                open={isModalOpen}
-                onOk={handleOk}
-                onCancel={handleCancel}
-            >
-                <div>Tên người dùng</div>
-                <Input placeholder="Tên người dùng" name='fullName' onChange={(e) => setFullName(e.target.value)} />
-                <div>Email</div>
-                <Input placeholder="Email" name='email' onChange={(e) => setEmail(e.target.value)} />
-                <div>password</div>
-                <Input type='password' placeholder="password" name='password' onChange={(e) => setPassword(e.target.value)} />
-                <div>phone</div>
-                <Input placeholder="phone" name='phone' onChange={(e) => setPhone(e.target.value)} />
-            </Modal>
-        </div>
+            {contextHolder}
+        </>
     );
 }
 
